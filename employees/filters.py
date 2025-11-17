174d22c3880c72be3e709f27ec0f6771e04a7e27
Peter <ngugipeter8922gmail.com>
2025-11-17 22:26:54 +0300

    done

---
+++ employees/filters.py
@@ -0,0 +1,23 @@
+import django_filters
+from .models import Employee    
+
+
+class EmployeeFilter(django_filters.FilterSet):
+    emp_department = django_filters.CharFilter(field_name='emp_department', lookup_expr='iexact')
+    emp_name = django_filters.CharFilter(field_name='emp_name', lookup_expr='icontains')
+    #id = django_filters.NumericRangeFilter(field_name='id')
+    id_min = django_filters.CharFilter(method='filter_by_id_range', label='From EMP ID')
+    id_max = django_filters.CharFilter(method='filter_by_id_range', label='To EMP ID')
+
+    
+    class Meta:
+        model = Employee
+        fields = ['emp_department', 'emp_name', 'id_min', 'id_max']
+
+
+    def filter_by_id_range(self, queryset, name, value):
+        if name == 'id_min':
+            return queryset.filter(emp_id__gte=value)
+        elif name == 'id_max':
+            return queryset.filter(emp_id__lte=value)
+        return queryset
